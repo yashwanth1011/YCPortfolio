e89e3f488092a0f714f80c7794549c60c41cacdc
yashwanth1011 <yashwanthchennu1999@gmail.com>
2024-10-17 23:32:31 -0400

    Changing About Me

---
+++ src/Pages/Home/AboutMeMatter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AboutMeMatter = () => {
+    return (
+        <p className="about--section-description">
+            I am a highly motivated <strong>Full Stack Developer</strong> and <strong>Machine Learning Enthusiast</strong> with over
+            <strong> 2 years of professional experience</strong> in developing scalable and optimized applications. I have worked as a <strong>Software Engineer at TATA Consultancy Services</strong>, where I successfully delivered
+            high-performance solutions for clients like <strong>DBS Bank</strong> and <strong>Santos Ltd</strong>.
+            My experience spans across <strong>front-end</strong> and <strong>back-end development</strong>, where I specialize
+            in technologies such as <strong>React</strong>, <strong>Node.js</strong>, <strong>Spring Boot</strong>, and <strong>AWS</strong>.<br />
+            I have also contributed to impactful <strong>Machine Learning projects</strong>, including <strong>Natural Language Query
+                to SQL Conversion</strong>, achieving 90% accuracy using <strong>BERT</strong> and <strong>LSTM models</strong>, and
+            <strong> Ethereum Fraud Detection</strong>, where I developed an ensemble learning model with 92% accuracy.<br />
+            I thrive in dynamic environments, balancing development and data science, and am passionate about solving real-world
+            problems through innovative solutions.
+
+
+        </p>
+    )
+}
+
+export default AboutMeMatter
--- src/Pages/Home/Timeline.jsx
+++ src/Pages/Home/Timeline.jsx
@@ -1,16 +1,55 @@
 import {
     VerticalTimeline,
     VerticalTimelineElement
-  } from "react-vertical-timeline-component";
+} from "react-vertical-timeline-component";
 
 import "react-vertical-timeline-component/style.min.css";
+import { FaGraduationCap } from "react-icons/fa6";
+import { MdWork } from "react-icons/md";
+
 
 import React from 'react'
 
 const Timeline = () => {
-  return (
-    <div>Timeline</div>
-  )
+    return (
+        <section className="timeline-section" >
+            <VerticalTimeline lineColor="black" animate="true">
+                <VerticalTimelineElement className="vertical-timeline-element--education"
+                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                    date={<span style={{ color: 'black' }}>Aug 2023 - present</span>}
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    icon={<FaGraduationCap />}
+                    
+                    >
+                    Master's in Computer Science and Engineering <br/>
+                    University at Buffalo
+                </VerticalTimelineElement>
+                <VerticalTimelineElement className="vertical-timeline-element--work"
+                    contentStyle={{ background: 'orange', color: '#fff' }}
+                    contentArrowStyle={{ borderRight: '7px solid  orange' }}
+                    date={<span style={{ color: 'black' }}>Aug 2021 - Aug 2023</span>}
+                    iconStyle={{ background: 'orange', color: '#fff' }}
+                    icon={<MdWork />}>
+                    Software Engineer at TCS
+                    <br/> Clients: Santos, DBS
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement className="vertical-timeline-element--education"
+                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                    date={<span style={{ color: 'black' }}>Aug 2021 - Aug 2023</span>}
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    icon={<FaGraduationCap />}>
+                    Bachelors of Technology
+                    <br/>
+                    Sreenidhi Institute of Science and Technology
+                
+                </VerticalTimelineElement>
+
+            </VerticalTimeline>
+        </section>
+    )
 }
 
 export default Timeline
